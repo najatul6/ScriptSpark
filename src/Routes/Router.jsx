afc9bc59858c4503fbff9a5c0996282778292583
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-01-29 01:44:53 +0600

    feat: restructure Router component to include new routes for LinkToScript, ScriptToComment, ImageToComment, and ClientReply

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -7,46 +7,65 @@ import Overview from "@/Pages/Dashboard/Overview";
 import Home from "@/Pages/Home/Home";
 import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
+import LinkToScript from "@/Pages/LinkToScript/LinkToScript";
+import ScriptToComment from "@/Pages/ScriptComment/ScriptToComment";
+import ImageToComment from "@/Pages/ImageToComment/ImageToComment";
+import ClientReply from "@/Pages/ClientReply/ClientReply";
 
 const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <MainLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-
-            },
-            {
-                path: "signin",
-                element: <Signin />
-            },
-            {
-                path: "signUp",
-                element: <Signup />
-            }
-        ]
-    },
-    {
-        path: "/dashboard",
-        element: <PrivateRoute>
-            <DashboardLayout />
-        </PrivateRoute>,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "overview",
-                element: <Overview />
-            }
-        ]
-    }
-    // {
-    //     path:"/register",
-    //     element:<Register/>
-    // }
-])
+        element: <Home />,
+      },
+      {
+        path: "link-to-script",
+        element: <LinkToScript />,
+      },
+      {
+        path: "script-to-comment",
+        element: <ScriptToComment />,
+      },
+      {
+        path: "image-to-comment",
+        element: <ImageToComment />,
+      },
+      {
+        path: "client-reply",
+        element: <ClientReply />,
+      },
+      {
+        path: "signin",
+        element: <Signin />,
+      },
+      {
+        path: "signUp",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "overview",
+        element: <Overview />,
+      },
+    ],
+  },
+  // {
+  //     path:"/register",
+  //     element:<Register/>
+  // }
+]);
 export default Router;
